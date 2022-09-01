cb62fd860c0edaf51a205b1ad589f2a22b2de100
renqingyang <rq625230882@gmail.com>
2022-09-01 11:39:23 -0500

    microservice

--- src/main/java/com/example/java17il2022/week5/Microservice.java
+++ src/main/java/com/example/java17il2022/week5/Microservice.java
@@ -15,8 +15,24 @@ package com.example.java17il2022.week5;
  *
  *      how
  *      1. api gateway
- *          global unique id
+ *          global unique id(log id, tx id..)
+ *              UUID
+ *              Database -> primary key
+ *              Snowflake ->  timestamp + machine id + xx id + serial id
  *          rate limiter
+ *              1. limit total request amount (token bucket)
+ *
+ *    Token generator  ----
+ *                        |
+ *                  \ O   X /
+ *                   \_____/   -----> user
+ *
+ *              2. based on IP  1 request / s
+ *
+ *                   timestamp -> ->
+ *                window [ip1, ip2, ip3 .... ]
+ *
+ *
  *      2. discovery service / service registration
  *              key: service name value: [ip1, ip2, ip3]
  *                 /
@@ -32,4 +48,37 @@ package com.example.java17il2022.week5;
  *          CAP -> sharding / scalability
  *      9. ci/cd
  *      10. container / linux
+ *      11. centralized log service
+ *       *       *       *       *       *       *       *       *       *       *       *       *       *       *       *       *       *       *
+ *      1. what is a good api
+ *          a. short response time / good performance
+ *          b. handle different corner cases
+ *          c. error handling + log
+ *          d. security
+ *          e. good documentation
+ *          f. follow SOLID + rest api standard
+ *          g. test
+ *
+ *      2. how do you start implementing restful api (TDD : test driven development)
+ *          a. get requirements
+ *          b. analyze requirements
+ *          c. consider corner cases
+ *          d. restful api(endpoint + request body / ..) + interface + abstract class + //TODO
+ *          e. impl all test cases
+ *          f. impl the TODO
+ *          g. run test cases
+ *          10. send pull request code review + merge code -> trigger jenkins pipeline
+ *
+ *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  homework
+ *      1. provide retry features in employee service (3 times)
+ *      2. return customized exception to search service
+ *      3. search service provide hystrix -> return default result / error msg (under close status)
+ *      4. provide global exception handling in search service (controller advice + exception handler -> return 500 error code)
+ *      5. learn message queue
+ *         explain kafka partition + broker + consumer group + ack number in txt file
+ *         explain diff between kafka and rabbit mq in txt file
+ *  DeadLine : before Tuesday
+ *
+
  */
