d4b9313d51554cf9ac460a81548eef150410669c
renqingyang <rq625230882@gmail.com>
2022-09-12 11:07:27 -0500

    aws basic

--- src/main/java/com/example/java17il2022/week6/Review.java
+++ src/main/java/com/example/java17il2022/week6/Review.java
@@ -29,16 +29,57 @@ package com.example.java17il2022.week6;
  *
  *  -> AWS introduction next week
  *      region, zone
- *      1. EC2 + ASG
- *      2. Application loadbalancer
- *      3. Route53
- *      4. S3 + S3 glacier
- *      5. RDS
+ *      1. EC2 + ASG    ASG[EC2-1,  EC2-2,  EC2-3]
+ *      2. Application Loadbalancer
+ *                   ALB
+ *                 /    \  /path
+ *             target   ASG
+ *
+ *      3. Route53 (DNS)
+ *      4. S3 <-> S3 glacier
+ *          /xx/xxx/file
+ *         a. image / video
+ *         b. log
+ *      5. RDS(Mysql, Postgre, Oracle)
  *      6. SQS(dead letter queue / FIFO / standard queue)  + SNS
  *      7. (Network Access Control List)VPC(public / private subnet,  ip range) + NAT instance + internet gateway(public ip access)
+ *
+ *                                    internet gateway
+ *                                      route table
+ *            private subnet[  EC2-1 ]              public  subnet[  EC2 - 2]
+ *            ip range ip1 ~ ip5(CIDR)              CIDR block
+ *
  *      8. ECS + ECR + docker container
+ *         ECR(docker image repository)
+ *         ECS + task definition
+ *                  ECS(centralized controller)
+ *
+ *        docker container1             docker container3
+ *        docker container2             docker container4
+ *         EC2 - 1                          EC2 - 2
+ *      (installed agent)                (installed agent)
+ *
+ *
+ *                  ECS(centralized controller)
+ *
+ *        docker container1             docker container3
+ *        docker container2             docker container4
+ *         Fargate-1                        Fargate-2
+ *
+ *
  *      9. IAM
- *      10. security group
- *      11. aws lambda
- *  Tomorrow : review interview questions
+ *                  root account(do not use / share this account)
+ *               /      \           \
+ *            account1  account2    account3
+ *          role list1    2             3
+ *      10. security group(firewall)
+ *      11. user(NY) -> edge location(NY data center) -> website(other country)
+ *                     cache the result from that website
+ *      12. KMS
+ *
+ *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+ *  1. download google chat in your phone (turn on the notification)
+ *  2. login your marketing email
+ *  3. add renqing.yang@antra.com
+ *
  */
